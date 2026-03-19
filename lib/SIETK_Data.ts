6ddb440ef6aecde8786aac17c179f62eeb3b7fa5
jailadeen149 <jailadeen149@gmail.com>
2026-03-19 12:24:20 +0000

    Data adding

---
+++ lib/SIETK_Data.ts
@@ -0,0 +1,133 @@
+
+export const SIETK_DATA = {
+  about: {
+    title: "ABOUT SIDDHARTH INSTITUTE OF ENGINEERING & TECHNOLOGY",
+    description: "Siddharth Institute of Engineering & Technology (SIETK) is an emerging center for excellence in Engineering & Management education, boast of energetic & experienced faculty, successful students, great infrastructure and excellent placements record. Established in 2001 in a sprawling 30-acre campus, the institutions are located in Puttur town and are very proximate to Narayanavanam. The institutions are 22kms from Tirupati airport and are well connected by rail and road.",
+    chairman: {
+      name: "Dr. K. Ashok Raju, M.A., Ph.D",
+      title: "Chairman of the Board",
+      description: "An educationist and philanthropist, spearheaded the Siddharth Group of Engineering Institutions to become emerging centres for excellence in Engineering and Management education."
+    },
+    viceChairman: {
+        name: "Dr. K. Indira Veni",
+        title: "Vice-Chairperson",
+        description: "An Electronics and Communication Engineer by education, and a doctorate from Sunrise University, Dr. Indira Veni believes in continuous education. She has been the Proprietor of Indasri fuel and service center, Srikalahasti and a Partner in Elite Tours & Travels, Tirupati."
+    },
+  },
+  academics: {
+    departments: [
+      "Civil Engineering",
+      "Electrical and Electronics Engineering",
+      "Mechanical Engineering",
+      "Electronics & Communication Engineering",
+      "Computer Science and Engineering",
+      "CSE(Artificial Intelligence & Machine Learning)",
+      "CSE(IOT & CS including BCT)",
+      "CSE(Artificial Intelligence & Data Science)",
+      "CSE(Artificial Intelligence)",
+      "CSE With Specialization In Cloud Computing",
+      "Computer Science & Information Technology",
+      "Agricultural Engineering",
+      "Basic Sciences & Humanities",
+      "MBA",
+      "MCA"
+    ],
+    programs: {
+      undergraduate: {
+        count: 12,
+        title: "Undergraduate Programs"
+      },
+      postgraduate: {
+        count: 7,
+        title: "Postgraduate Programs"
+      },
+      diploma: {
+        count: 5,
+        title: "Diploma Programmes"
+      }
+    },
+    accreditations: [
+        {
+            name: "NBA (National Board of Accreditation)",
+            details: "Accredited for five programmes."
+        },
+        {
+            name: "NAAC",
+            details: "Accredited with ‘A’ grade."
+        }
+    ]
+  },
+  faculty: {
+      mechanical: [
+        { name: "Dr.S Sunil Kumar Reddy", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.C Sreedhar", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.Dara Sudhakara", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.F Anand Raju", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.Sonagiri Suresh", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.B Vinod", designation: "Associate Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.Palamangalam Ramesh", designation: "Associate Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.Sreenivasulu Bezawada", designation: "Associate Professor", qualification: "M.E., Ph.D." },
+        { name: "Dr.Anandan B", designation: "Associate Professor", qualification: "M.E., Ph.D" },
+        { name: "Dr. Renangi Sandeep", designation: "Associate Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr.Mani J", designation: "Associate Professor", qualification: "M.E., Ph.D." },
+        { name: "Dr.Parla Jaya Prakash", designation: "Associate Professor", qualification: "M.Tech., Ph.D." },
+        { name: "Darsi Kishnaiah", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Karthikeyan V", designation: "Assistant Professor", qualification: "M.E." },
+        { name: "K Sai Sujith", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Akula Suresh", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Giriprasath P R", designation: "Assistant Professor", qualification: "M.E." },
+        { name: "Sai Prasad. Kanchi", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Shankar L", designation: "Assistant Professor", qualification: "M.E." },
+        { name: "Asha A", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "J Suresh", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "TB Haribabu", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Patnool Shaik Mohammed Sharook", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "B A Devan", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "A R Inbaraj", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Mayaluri Sudheer", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "GV Dellibabu", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "P Vikram", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "T Cholairaj", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "V S Ravi", designation: "Assistant Professor", qualification: "M.Tech." }
+      ],
+    electrical: [
+        { name: "Dr. N RAMESH RAJU", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr. J GOWRISHANKAR", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr. N Balavenkata Muni", designation: "Professor", qualification: "M. Tech., Ph.D." },
+        { name: "Dr. S Ramesh", designation: "Professor", qualification: "M.E., Ph.D." },
+        { name: "Dr. Keelagaram Gunaprasad", designation: "Professor", qualification: "M.Tech., Ph.D" },
+        { name: "Dr. Munireddy G", designation: "Professor", qualification: "M. Tech., Ph.D." },
+        { name: "Dr. S Narasimha Rao", designation: "Professor", qualification: "M.E., Ph.D" },
+        { name: "Dr. Muni Sekhar P", designation: "Professor", qualification: "M.Tech.,Ph.D" },
+        { name: "Dr. Lakshmi R", designation: "Associate Professor", qualification: "M.E., Ph.D." },
+        { name: "Dr. Kola Muralikumar", designation: "Associate Professor", qualification: "M. Tech., Ph.D." },
+        { name: "Dr. Devalraju Prasad", designation: "Associate Professor", qualification: "M. Tech., Ph.D." },
+        { name: "Dr. M Priya", designation: "Associate Professor", qualification: "M. Tech., Ph.D." },
+        { name: "Dr. Divakar Kuruna", designation: "Associate Professor", qualification: "M.Tech., Ph.D." },
+        { name: "V Siva Subramanyam", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Jyothsna Priya V", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Mamatha V", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "V N SARASWATHI", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Bindhu Madhavi N", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Hari A", designation: "Assistant Professor", qualification: "M.E." },
+        { name: "R S Sai Praveen Kumar", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "K Vishnu", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Koneti Babu", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Boggala Ramesh", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Sravanthi Narasambattu", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "M Kalaivani", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Peta Nandhini", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Sadisi Yogesh", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "Manikyam Rajyalakshmi", designation: "Assistant Professor", qualification: "M.Tech." },
+        { name: "M Supriya", designation: "Assistant Professor", qualification: "M.E." },
+        { name: "Kodakala Rajkumar", designation: "Assistant Professor", qualification: "M.Tech." }
+    ]
+  },
+  contact: {
+    phone: "08577-264999",
+    fax: "08577-264888",
+    email: "sietk.puttur@gmail.com",
+    studentVerificationEmail: "sietkverify@siddharthgroup.ac.in",
+    address: "Siddharth Nagar, Narayana Vanam Road,Puttur, Chittoor District, Andhra Pradesh, India. PIN:517583"
+  }
+};
